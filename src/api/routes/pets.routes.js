bf230d9d8146d2989ca96ab7c672d888f649b99e
DavidRodriguezDev <contacto.davidrodriguezdiaz@gmail.com>
2022-12-07 12:57:51 +0100

    init

--- src/api/routes/pets.routes.js
+++ src/api/routes/pets.routes.js
@@ -6,7 +6,7 @@ const {isAuth} = require("../../middlewares/auth")
 
 const {getAllPets, getPet, postNewPet, putPet, deletePet} = require("../controllers/pets.controllers");
 
-routerPets.get('/',[isAuth], getAllPets);        //GET Creamos todas las funciones, las exportamos y las anexionamos al router correspondiente
+routerPets.get('/', getAllPets);        //GET Creamos todas las funciones, las exportamos y las anexionamos al router correspondiente
 routerPets.get('/:id', getPet);         //GET Unitario por id
 routerPets.post('/', postNewPet);          //POST Para crear un nuevo elemento a la base de datos.
 routerPets.put('/:id', putPet);     //PUT Para modificar un elemento de la base de datos recibiendo como parámetro un id
